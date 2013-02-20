13c6269be3d35e1bb267404c0ac2e6d897dcb9cc
andydenenberg <andydenenberg@gmail.com>
2013-02-20 10:38:48 -0600

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "Ftv4"
+set :repository,  "git@github.com:andydenenberg/ftv4.git"
+set :deploy_to, "/var/www/ftv4"
+set :user, "ubuntu"
+set :keep_releases, 5
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "10.96.30.86"                          # Your HTTP server, Apache/etc
+role :app, "10.96.30.86"                          # This may be the same as your `Web` server
+role :db,  "10.96.30.86", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
